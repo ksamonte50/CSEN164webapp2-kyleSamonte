49abe89eedcd9941d14c1aaf596880806133b295
ksamonte50 <ksamonte50@gmail.com>
2025-06-11 23:28:54 -0700

    Refactor CartsController to include CurrentCart concern and update cart view to display line items with quantity

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy ]
+  # before_action :set_cart, only: %i[ show edit update destroy ]
+  include(CurrentCart)
+  before_action :set_cart
 
   # GET /carts or /carts.json
   def index
