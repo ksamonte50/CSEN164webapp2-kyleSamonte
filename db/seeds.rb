b6ea16777e2082d2bef7bb2279d19b0d0e89937b
ksamonte50 <ksamonte50@gmail.com>
2025-06-11 18:34:30 -0700

    Seed database with initial product records

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.delete_all
+Product.create!(
+    name: "Nike Air Max 1",
+    description: "The Nike Air Max 270 is a stylish and comfortable sneaker that features a large Air unit for maximum cushioning.",
+    image: 'AM1.jpg',
+    price: 150.00
+)
+
+Product.create!(
+    name: "Air Jordan 1 Retro High OG",
+    description: "The Air Jordan 1 Retro High OG is a classic sneaker that features a high-top design and premium materials.",
+    image: 'AJ1.jpg',
+    price: 180.00
+)
+
+Product.create!(
+    name: "New Balance 990v5",
+    description: "The New Balance 990v5 is a comfortable and supportive sneaker that features a classic design and premium materials.",
+    image: 'NB990.jpg',
+    price: 185.00
+)
