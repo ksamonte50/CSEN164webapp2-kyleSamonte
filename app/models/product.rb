cd2bf3bcd9eb1297e36753fbf824c1dac8fe3fa0
ksamonte50 <ksamonte50@gmail.com>
2025-06-11 20:09:44 -0700

    Add item constraints

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+    # Constraints
+    validates(:name, :description, :image, :price, presence: true)
+    validates(:price, numericality: { greater_than_or_equal_to: 0.01 })
+    validates(:image, format: {with: %r{\.(png|jpg|jpeg|gif)\Z}, message: "must be a valid image format (png, jpg, jpeg, gif)"})
+    validates(:name, uniqueness: true)
 end
