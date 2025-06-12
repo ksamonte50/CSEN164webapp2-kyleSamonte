ae5b6218b4d10986038e033b03134916e6add353
ksamonte50 <ksamonte50@gmail.com>
2025-06-11 21:46:05 -0700

    Add lineitems association to Cart model and implement before_destroy callback in Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,14 @@ class Product < ApplicationRecord
     validates(:price, numericality: { greater_than_or_equal_to: 0.01 })
     validates(:image, format: {with: %r{\.(png|jpg|jpeg|gif)\Z}, message: "must be a valid image format (png, jpg, jpeg, gif)"})
     validates(:name, uniqueness: true)
+
+    # Register Call-back function
+    before_destroy(:make_sure_no_line_item)
+    def make_sure_no_line_item
+        if lineitems.empty? #self.lineitems.empty?
+            return true
+        else
+            return false
+        end
+    end
 end
